dd0afd2621acd859759de60295ee87eaa92745ac
Nick Doiron <ndoiron@mapmeld.com>
2014-08-13 19:19:48 -0400

    experimenting with maps

---
+++ README.md
@@ -0,0 +1,19 @@
+# Van Gogh Map
+
+Experiment with MapBox's new WebGL mapping library
+
+HTML + JS
+
+Doesn't work if your browser is IE or a potato.
+
+## How it works
+
+Several Van Gogh painting scans are available as textures
+
+You select textures to make up the basemap
+
+MapBox GL renders a new map based on your selected textures
+
+## License
+
+GPLv3+ deal with it
---
+++ map.js
@@ -0,0 +1,20 @@
+mapboxgl.accessToken = "pk.eyJ1IjoibWFwbWVsZCIsImEiOiI0a1NzYW53In0.2gQTd6k9Ghw8UBK4DsciLA";
+var map = new mapboxgl.Map({
+    container: 'map',
+    center: [42, -71],
+    zoom: 15,
+    style: "mss.json",
+    hash: true
+});
+
+var source = new mapboxgl.Source({
+  type: "vector",
+  url: "mapbox://mapbox.mapbox-streets-v5",
+  tileSize: 512
+});
+map.addSource('osm', source);
+
+$("select").on("change", function(e){
+  var layer = $(e.target).attr("id");
+  map.setStyle({});
+});
